f82f04b8b05d009d52853adcd495c22f17b653cc
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-07-20 21:33:35 +0600

    added videoplayer

--- videoApp/urls.py
+++ videoApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
   
-from videoApp.views import VideoListView, VideoView
+from videoApp.views import VideoListView, VideoView, VideoStreamView
 
 urlpatterns = [
     path("", VideoListView.as_view(), name="home"),
-    path("video/<int:pk>/", VideoView.as_view(), name="video")
+    path("video/<int:pk>/", VideoView.as_view(), name="video"),
+    path("video/stream/<int:pk>/", VideoStreamView, name="stream")
 ]
--- videoApp/views.py
+++ videoApp/views.py
@@ -1,6 +1,8 @@
+from django.http.response import StreamingHttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 
+from videoApp.stream import VideoStream
 from videoApp.models import Video
 
 class VideoListView(generic.View):
@@ -20,3 +22,17 @@ class VideoView(generic.View):
             "videoApp/video.html",
             {"video": get_object_or_404(Video, pk=pk)}
         )
+    
+def VideoStreamView(request, pk):
+    video_stream = VideoStream()
+    file, status_code, content_length, content_range = video_stream.open_file(request, pk)
+    response = StreamingHttpResponse(file, status=status_code, content_type='video/mp4')
+
+    # Headers for front-end
+    response['Accept-Ranges'] = 'bytes'
+    response['Content-Length'] = str(content_length)
+    response['Cache-Control'] = 'no-cache'
+    response['Content-Range'] = content_range
+    return response
+
+#TODO 1 Сделать пагинацию homeview. 2 Сделать html для video.html. 3 Сделать систему комментов с django_channels
